3b5e75fdd0b8aaf3bf045f261c55f868323eaed0
shijiawei124 <506437858@qq.com>
2020-12-08 11:42:59 +0800

    'modified'

--- configs/_base_/datasets/coco_detection.py
+++ configs/_base_/datasets/coco_detection.py
@@ -1,5 +1,4 @@
 dataset_type = 'CocoDataset'
-data_root = 'data/coco/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
@@ -32,17 +31,17 @@ data = dict(
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/instances_train2017.json',
-        img_prefix=data_root + 'train2017/',
+        ann_file= '/content/drive/MyDrive/anther_final_data/with_two_classes_json/train.json',
+        img_prefix= '/content/drive/MyDrive/anther_final_data/train_imgs/',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/instances_val2017.json',
-        img_prefix=data_root + 'val2017/',
+        ann_file='/content/drive/MyDrive/anther_final_data/with_two_classes_json/val.json',
+        img_prefix='val2017/',
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/instances_val2017.json',
-        img_prefix=data_root + 'val2017/',
+        ann_file='/content/drive/MyDrive/anther_final_data/with_two_classes_json/val.json',
+        img_prefix='/content/drive/MyDrive/anther_final_data/val_imgs/',
         pipeline=test_pipeline))
 evaluation = dict(interval=1, metric='bbox')
--- configs/_base_/schedules/schedule_20e.py
+++ configs/_base_/schedules/schedule_20e.py
@@ -7,5 +7,5 @@ lr_config = dict(
     warmup='linear',
     warmup_iters=500,
     warmup_ratio=0.001,
-    step=[16, 19])
-total_epochs = 20
+    step=[75, 90])
+total_epochs = 100
